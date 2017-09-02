9f66b3493f4db416ffad595734e7d3646a1b459a
Denis <denis.vasilin@gmail.com>
2017-09-02 16:15:43 +0300

    CoverageFragment updated.

--- app/src/main/java/com/teambrella/android/ui/user/coverage/CoverageFragment.java
+++ app/src/main/java/com/teambrella/android/ui/user/coverage/CoverageFragment.java
@@ -10,9 +10,13 @@ import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.teambrella.android.R;
+
+import java.util.Random;
 
 /**
  * Coverage fragment
@@ -20,6 +24,11 @@ import com.teambrella.android.R;
 public class CoverageFragment extends Fragment {
 
     private TextView mCoverageView;
+    private ImageView mCoverageIcon;
+    private TextView mMaxExpenses;
+    private TextView mPossibleExpenses;
+    private TextView mTeamPay;
+    private SeekBar mSeekBar;
 
     @Nullable
     @Override
@@ -28,15 +37,31 @@ public class CoverageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_coverage, container, false);
 
         mCoverageView = view.findViewById(R.id.coverage);
+        mCoverageIcon = view.findViewById(R.id.coverage_icon);
+        mMaxExpenses = view.findViewById(R.id.max_expenses_value);
+        mPossibleExpenses = view.findViewById(R.id.possible_expenses_value);
+        mTeamPay = view.findViewById(R.id.team_pay_value);
+        mSeekBar = view.findViewById(R.id.seek_bar);
 
-
-        SpannableString coverage = new SpannableString("10%");
-
-        coverage.setSpan(new ForegroundColorSpan(getContext().getResources().getColor(R.color.darkSkyBlue)), 2, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        coverage.setSpan(new RelativeSizeSpan(0.2f), 2, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        mCoverageView.setText(coverage);
-
+        setCoverage(new Random().nextInt(101));
         return view;
+    }
+
+
+    private void setCoverage(int coverage) {
+        String coverageString = Integer.toString(coverage);
+        SpannableString coveragePercent = new SpannableString(coverage + "%");
+        coveragePercent.setSpan(new ForegroundColorSpan(getContext().getResources().getColor(R.color.darkSkyBlue)), coverageString.length(), coverageString.length() + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        coveragePercent.setSpan(new RelativeSizeSpan(0.2f), coverageString.length(), coverageString.length() + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mCoverageView.setText(coveragePercent);
+
+        if (coverage > 97) {
+            mCoverageIcon.setImageResource(R.drawable.cover_sunny);
+        } else if (coverage > 90) {
+            mCoverageIcon.setImageResource(R.drawable.cover_lightrain);
+        } else {
+            mCoverageIcon.setImageResource(R.drawable.cover_rain);
+        }
+
     }
 }
