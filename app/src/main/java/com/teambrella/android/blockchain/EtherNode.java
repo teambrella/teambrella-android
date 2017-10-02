3a9abf9b2882c2df14f43901d818fe4d9a1be0f6
porub <porubaev@gmail.com>
2017-10-02 11:19:22 +0300

    activating main net

--- app/src/main/java/com/teambrella/android/blockchain/EtherNode.java
+++ app/src/main/java/com/teambrella/android/blockchain/EtherNode.java
@@ -27,9 +27,7 @@ public class EtherNode {
     private final List<EtherAPI> mEtherAPIs = new LinkedList<>();
     private final boolean mIsTestNet;
     private static final String[] TEST_AUTHORITIES = new String[]{"https://ropsten.etherscan.io"};
-    //// XXX: TODO: uncomment for unblocking mainnet:
-    //private static final String[] MAIN_AUTHORITIES = new String[]{"http://api.etherscan.io"};
-    private static final String[] MAIN_AUTHORITIES = new String[]{"https://etherscan.io"};
+    private static final String[] MAIN_AUTHORITIES = new String[]{"http://api.etherscan.io"};
 
     public EtherNode(boolean testNet) {
         this.mIsTestNet = testNet;
