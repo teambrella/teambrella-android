e7bac6bea4c196648b1916d644dc773afb364342
Denis <denis.vasilin@gmail.com>
2017-10-24 14:58:15 +0300

    Version Up!

--- app/src/main/java/com/teambrella/android/backup/TeambrellaBackupAgent.java
+++ app/src/main/java/com/teambrella/android/backup/TeambrellaBackupAgent.java
@@ -1,7 +1,14 @@
 package com.teambrella.android.backup;
 
 import android.app.backup.BackupAgentHelper;
+import android.app.backup.BackupDataInput;
+import android.app.backup.BackupDataOutput;
 import android.app.backup.SharedPreferencesBackupHelper;
+import android.os.ParcelFileDescriptor;
+
+import com.teambrella.android.util.StatisticHelper;
+
+import java.io.IOException;
 
 /**
  * Teambrella Backup Agent
@@ -15,5 +22,17 @@ public class TeambrellaBackupAgent extends BackupAgentHelper {
         super.onCreate();
         SharedPreferencesBackupHelper helper = new SharedPreferencesBackupHelper(this, TeambrellaBackupData.NAME);
         addHelper(PREFS_BACKUP_KEY, helper);
+    }
+
+    @Override
+    public void onBackup(ParcelFileDescriptor oldState, BackupDataOutput data, ParcelFileDescriptor newState) throws IOException {
+        super.onBackup(oldState, data, newState);
+        StatisticHelper.onBackUp();
+    }
+
+    @Override
+    public void onRestore(BackupDataInput data, int appVersionCode, ParcelFileDescriptor newState) throws IOException {
+        super.onRestore(data, appVersionCode, newState);
+        StatisticHelper.onRestore();
     }
 }
