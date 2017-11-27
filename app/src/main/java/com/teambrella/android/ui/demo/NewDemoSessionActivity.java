a90aabefe5597dfc521d9cca3e5fc13ac6022de5
Denis <denis.vasilin@gmail.com>
2017-11-27 19:54:07 +0300

    New Demo Session Activity.

---
+++ app/src/main/java/com/teambrella/android/ui/demo/NewDemoSessionActivity.java
@@ -0,0 +1,19 @@
+package com.teambrella.android.ui.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.teambrella.android.R;
+
+/**
+ * New Demo Session Error Screen.
+ */
+public class NewDemoSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_demo_session);
+    }
+}
