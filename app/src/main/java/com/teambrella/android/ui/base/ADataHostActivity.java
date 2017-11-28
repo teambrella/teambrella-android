84f1fc29c707ecce6631d3d9ed2e22776989531e
Denis <denis.vasilin@gmail.com>
2017-11-28 13:28:12 +0300

    Start New Demo Session Screen.

--- app/src/main/java/com/teambrella/android/ui/base/ADataHostActivity.java
+++ app/src/main/java/com/teambrella/android/ui/base/ADataHostActivity.java
@@ -1,5 +1,6 @@
 package com.teambrella.android.ui.base;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
@@ -8,18 +9,31 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.teambrella.android.api.TeambrellaModel;
+import com.teambrella.android.api.TeambrellaServerException;
 import com.teambrella.android.data.base.IDataHost;
 import com.teambrella.android.data.base.IDataPager;
 import com.teambrella.android.data.base.TeambrellaDataFragment;
 import com.teambrella.android.data.base.TeambrellaDataPagerFragment;
+import com.teambrella.android.ui.TeambrellaUser;
+import com.teambrella.android.ui.demo.NewDemoSessionActivity;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import io.reactivex.Notification;
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 /**
  * A Data Host Activity
  */
 public abstract class ADataHostActivity extends AppCompatActivity implements IDataHost {
+
+
+    private List<Disposable> mNewSessionDisposables = new LinkedList<>();
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -69,6 +83,57 @@ public abstract class ADataHostActivity extends AppCompatActivity implements IDa
             return dataFragment.getPager();
         }
         return null;
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TeambrellaUser user = TeambrellaUser.get(this);
+        if (user.isDemoUser()) {
+            String[] dataTags = getDataTags();
+            if (dataTags != null && dataTags.length > 0) {
+                for (String tag : dataTags) {
+                    mNewSessionDisposables.add(getObservable(tag).subscribe(this::checkDemoAuthError));
+                }
+            }
+            String[] pagerTags = getPagerTags();
+            if (pagerTags != null && pagerTags.length > 0) {
+                for (String tag : pagerTags) {
+                    mNewSessionDisposables.add(getPager(tag).getObservable().subscribe(this::checkDemoAuthError));
+                }
+            }
+        }
+    }
+
+    private void checkDemoAuthError(Notification<JsonObject> notification) {
+        if (notification.isOnError()) {
+            Throwable error = notification.getError();
+            if (error instanceof TeambrellaServerException) {
+                TeambrellaServerException serverException = (TeambrellaServerException) error;
+                switch (serverException.getErrorCode()) {
+                    case TeambrellaModel.VALUE_STATUS_RESULT_CODE_AUTH:
+                        if (!isFinishing()) {
+                            startActivity(new Intent(this, NewDemoSessionActivity.class));
+                            finish();
+                        }
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Iterator<Disposable> iterator = mNewSessionDisposables.iterator();
+        while (iterator.hasNext()) {
+            Disposable disposable = iterator.next();
+            if (disposable != null && !disposable.isDisposed()) {
+                disposable.dispose();
+                iterator.remove();
+            }
+        }
     }
 
     @Override
