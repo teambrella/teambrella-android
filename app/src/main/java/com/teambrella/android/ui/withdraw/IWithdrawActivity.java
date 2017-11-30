7db7b809fc72c49a8206366c37345687e362f19f
Denis <denis.vasilin@gmail.com>
2017-11-30 18:17:55 +0300

    Withdraw Info Dialog Fragment.

---
+++ app/src/main/java/com/teambrella/android/ui/withdraw/IWithdrawActivity.java
@@ -0,0 +1,10 @@
+package com.teambrella.android.ui.withdraw;
+
+import com.teambrella.android.data.base.IDataHost;
+
+/**
+ * Interface for Withdraw Activity
+ */
+public interface IWithdrawActivity extends IDataHost {
+    void showWithdrawInfo();
+}
