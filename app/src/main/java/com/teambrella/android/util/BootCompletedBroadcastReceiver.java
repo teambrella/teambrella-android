335ec39f50d978835b57aae0c415a30a19d6ad00
Denis <denis.vasilin@gmail.com>
2017-12-06 16:51:22 +0300

    Translations updated.

--- app/src/main/java/com/teambrella/android/util/BootCompletedBroadcastReceiver.java
+++ app/src/main/java/com/teambrella/android/util/BootCompletedBroadcastReceiver.java
@@ -6,18 +6,25 @@ import android.content.Intent;
 
 import com.teambrella.android.services.TeambrellaNotificationService;
 import com.teambrella.android.ui.TeambrellaUser;
+import com.teambrella.android.util.log.Log;
 
 /**
  * Boot Completed broadcast receiver
  */
 public class BootCompletedBroadcastReceiver extends BroadcastReceiver {
 
+    private static final String LOG_TAG = BootCompletedBroadcastReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
         TeambrellaUser user = TeambrellaUser.get(context);
         String action = intent != null ? intent.getAction() : null;
         if (Intent.ACTION_BOOT_COMPLETED.equals(action) && !user.isDemoUser() && user.getPrivateKey() != null) {
-            context.startService(new Intent(context, TeambrellaNotificationService.class).setAction(TeambrellaNotificationService.CONNECT_ACTION));
+            try {
+                context.startService(new Intent(context, TeambrellaNotificationService.class).setAction(TeambrellaNotificationService.CONNECT_ACTION));
+            } catch (Exception e) {
+                Log.reportNonFatal(LOG_TAG, e);
+            }
         }
     }
 }
