423c193dffdaa59cde0dc2a28b6bcf90390b2212
Denis <denis.vasilin@gmail.com>
2018-02-12 15:31:52 +0300

    AppOutdatedActivity

---
+++ app/src/main/java/com/teambrella/android/ui/app/AppOutdatedActivity.java
@@ -0,0 +1,10 @@
+package com.teambrella.android.ui.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * App is outdated.
+ */
+public class AppOutdatedActivity extends AppCompatActivity {
+
+}
