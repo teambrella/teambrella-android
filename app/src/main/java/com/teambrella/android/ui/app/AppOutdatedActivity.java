7ba0c4a22e8a4e65005446248628ffeaee7caa2b
Denis <denis.vasilin@gmail.com>
2018-02-12 16:29:21 +0300

    Outdated activity layout.

--- app/src/main/java/com/teambrella/android/ui/app/AppOutdatedActivity.java
+++ app/src/main/java/com/teambrella/android/ui/app/AppOutdatedActivity.java
@@ -1,10 +1,19 @@
 package com.teambrella.android.ui.app;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.teambrella.android.R;
 
 /**
  * App is outdated.
  */
 public class AppOutdatedActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outdated);
+    }
 }
