4151a6b4e8ef40d051754b7436ed002528bceb17
Denis <denis.vasilin@gmail.com>
2018-03-05 22:08:04 +0300

    ReportNonFatal upon receiving messages

--- app/src/main/java/com/teambrella/android/services/TeambrellaFirebaseMessagingService.kt
+++ app/src/main/java/com/teambrella/android/services/TeambrellaFirebaseMessagingService.kt
@@ -4,6 +4,7 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.teambrella.android.backup.WalletBackUpService
 import com.teambrella.android.util.TeambrellaUtilService
+import com.teambrella.android.util.log.Log
 
 /**
  * Teambrella FireBase Messaging Service
@@ -11,6 +12,7 @@ import com.teambrella.android.util.TeambrellaUtilService
 class TeambrellaFirebaseMessagingService : FirebaseMessagingService() {
 
     companion object {
+        private const val LOG_TAG = "TeambrellaFirebaseMessagingService"
         private const val DEBUG_DB = "101"
         private const val DEBUG_UPDATE = "102"
         private const val DEBUG_SYNC = "103"
@@ -21,13 +23,29 @@ class TeambrellaFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         remoteMessage?.data?.let {
             when (it[CMD]) {
-                DEBUG_DB -> TeambrellaUtilService.scheduleDebugDB(this)
-                DEBUG_SYNC -> TeambrellaUtilService.oneoffWalletSync(this, (it[DEBUG]
-                        ?: "false").toBoolean(), true)
-                DEBUG_UPDATE -> TeambrellaUtilService.oneOffUpdate(this, (it[DEBUG]
-                        ?: "false").toBoolean())
+                DEBUG_DB -> {
+                    TeambrellaUtilService.scheduleDebugDB(this)
+                    Log.reportNonFatal(LOG_TAG, DebugDBMessagingException())
+                }
+                DEBUG_SYNC -> {
+                    TeambrellaUtilService.oneoffWalletSync(this, (it[DEBUG]
+                            ?: "false").toBoolean(), true)
+                    Log.reportNonFatal(LOG_TAG, DebugSyncMessagingException())
+                }
+                DEBUG_UPDATE -> {
+                    TeambrellaUtilService.oneOffUpdate(this, (it[DEBUG]
+                            ?: "false").toBoolean())
+                    Log.reportNonFatal(LOG_TAG, DebugUpdateMessagingException())
+                }
             }
         }
         WalletBackUpService.schedulePeriodicBackupCheck(this)
     }
+
+
+    open class TeambrellaFirebaseMessagingException(message: String) : Exception(message)
+    class DebugDBMessagingException : TeambrellaFirebaseMessagingException(DEBUG_DB)
+    class DebugSyncMessagingException : TeambrellaFirebaseMessagingException(DEBUG_SYNC)
+    class DebugUpdateMessagingException : TeambrellaFirebaseMessagingException(DEBUG_UPDATE)
+
 }
