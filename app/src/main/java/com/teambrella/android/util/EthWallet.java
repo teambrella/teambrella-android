9d1a5375d185fea2414ea0376ded531c3bd2f9bc
Denis <denis.vasilin@gmail.com>
2018-06-12 12:47:01 +0300

    Gas limit is updated for payments.

--- app/src/main/java/com/teambrella/android/util/EthWallet.java
+++ app/src/main/java/com/teambrella/android/util/EthWallet.java
@@ -262,7 +262,7 @@ public class EthWallet {
 
         Multisig myMultisig = tx.getFromMultisig();
         long myNonce = getMyNonce();
-        long gasLimit = 150_000L + 30_000L * tx.txOutputs.size();
+        long gasLimit = 150_000L + 55_000L * tx.txOutputs.size();
         long gasPrice = getGasPriceForClaim();
         String multisigAddress = myMultisig.address;
         String methodId = METHOD_ID_TRANSFER;
